6a8a12ed9c5121ac525d6ed0088eedc2ebd8293f
paconte <paconte@gmail.com>
2017-01-06 18:47:22 +0100

    Add cronjob to download jobs.

--- geekjobs/settings.py
+++ geekjobs/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'geekjobs',
+    'django_crontab',
     'widget_tweaks',
-    'formtools',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -138,3 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRONJOBS = [
+    ('0 0 * * *', 'geekjobs.stackoverflow_parser.download_stackoverflow_jobs', '>> /tmp/scheduled_job.log')
+]
